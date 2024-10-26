8af5608242929be48d1e627ba8e68c864fc64b7d
hussein <husseinsedoum@gmail.com>
2024-10-26 21:08:58 +0000

    another error need fixing for vercel deploy

--- lib/mongodb/mongodDBValidate.ts
+++ lib/mongodb/mongodDBValidate.ts
@@ -1,9 +1,8 @@
-// @ts-ignore
-import ObjectID from 'bson-objectid' 
+// @ts-expect-error: Importing ObjectID from 'bson-objectid' may not have type definitions
+import ObjectID from 'bson-objectid';
 
 function validateObjectId(storeCode: string): boolean {
-  return ObjectID.isValid(storeCode)
-  
+  return ObjectID.isValid(storeCode);
 }
 
-export default validateObjectId
+export default validateObjectId;
