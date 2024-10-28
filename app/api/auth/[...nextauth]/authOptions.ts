6df282fdf7b8c295ae9c5d30700155c524dad8f9
hussein <husseinsedoum@gmail.com>
2024-10-28 11:53:31 +0000

    trying to fix auth issue in dashboard

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -1,5 +1,3 @@
-// app/api/auth/authOptions.ts
-
 import GoogleProvider from "next-auth/providers/google";
 import type { NextAuthOptions } from "next-auth";
 
@@ -28,6 +26,18 @@ export const authOptions: NextAuthOptions = {
   jwt: {
     secret: process.env.NEXTAUTH_SECRET, // Ensure this secret is the same in both projects
   },
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "none", // Allow cross-origin
+        secure: true, // Ensure this is true in production
+        path: '/',
+        domain: ".vercel.app", // Allow access across subdomains
+      },
+    },
+  },
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
@@ -40,8 +50,8 @@ export const authOptions: NextAuthOptions = {
         if (!session.user) {
           session.user = {}; // Ensure session.user is defined
         }
-        // @ts-expect-error: Assigning user ID to session.user since TypeScript does not recognize session.user as a complete type.
-        session.user.id = token.id; // Assign user ID
+     // @ts-expect-error: Assigning user ID to session.user since TypeScript does not recognize session.user as a complete type.
+        session.user.id = token.id;    
       }
       return session;
     },
