e1aaf8bef11425462bad8cbe1e84ddfcec187206
hussein <husseinsedoum@gmail.com>
2024-10-29 16:53:01 +0000

    just making it without secure

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -25,15 +25,12 @@ export const authOptions: NextAuthOptions = {
   },
   cookies: {
     sessionToken: {
-      name: process.env.NODE_ENV === 'production' 
-        ? `__Secure-next-auth.session-token`
-        : `next-auth.session-token`,
+      name: `next-auth.session-token`,
       options: {
         // httpOnly: true,
         // sameSite: 'lax',
         path: '/',
         secure: process.env.NODE_ENV === 'production',
-        
       },
     },
   },
