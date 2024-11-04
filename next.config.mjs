8bba4dfbc8c1a4dccdc34db32275796fe36f303a
hussein <husseinsedoum@gmail.com>
2024-11-04 20:06:53 +0000

    adding these changes into site

--- next.config.mjs
+++ next.config.mjs
@@ -20,23 +20,28 @@ const nextConfig = {
     domain: '.vercel.app',
   },
   async headers() {
-    if (process.env.NODE_ENV == 'production')
-    return [
-      {
+    // Always return an array
+    const headersArray = [];
+
+    if (process.env.NODE_ENV === 'production') {
+      headersArray.push({
         source: '/api/auth/:path*',
         has: [{ type: 'header', key: 'Origin', value: '(?<serviceName>^https://.*.vercel.app$)' }],
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-            { key: 'Access-Control-Allow-Origin', value: ':origin' },
-            { key: 'Access-Control-Allow-Methods', value: 'GET, OPTIONS, PATCH, DELETE, POST, PUT' },
-            {
-              key: 'Access-Control-Allow-Headers',
-              value:
-                  'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-            },
+          { key: 'Access-Control-Allow-Origin', value: ':origin' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, OPTIONS, PATCH, DELETE, POST, PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
         ],
-      },
-    ];
+      });
+    }
+
+    // Return the headers array (it can be empty if not in production)
+    return headersArray;
   },
 };
 
