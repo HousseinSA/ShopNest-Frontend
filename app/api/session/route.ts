60f27a623c0728287e9c1f16ede1850d41e70a1d
hussein <husseinsedoum@gmail.com>
2024-11-04 19:10:03 +0000

    fixing req issue

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -2,7 +2,7 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions'; // Adjust this path based on your structure
 
-export async function GET(req: Request) {
+export async function GET() {
   const session = await getServerSession(authOptions);
 
   if (session) {
