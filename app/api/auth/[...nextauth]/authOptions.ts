722ea5023e5f11f696d44d36ef1182fc47c4e435
hussein <husseinsedoum@gmail.com>
2024-11-04 22:49:03 +0000

    testing

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -26,17 +26,18 @@ export const authOptions: NextAuthOptions = {
   },
   cookies: {
     sessionToken: {
-      name: process.env.NODE_ENV === 'production'
-         ? `__Secure-next-auth.session-token`
-         : `next-auth.session-token`,
+      name: process.env.NODE_ENV === 'production' 
+        ? `__Secure-next-auth.session-token`
+        : `next-auth.session-token`,
       options: {
         httpOnly: true,
         sameSite: 'lax',
         path: '/',
-        secure: process.env.NODE_ENV === 'production' ? true : false
-      }
+        secure:true,
+        // domain: '.vercel.app' 
+      },
     },
-   },
+  },
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
