67edd9663919022cd5e4a09562ddfbba08ccdc50
hussein <husseinsedoum@gmail.com>
2024-11-17 20:46:32 +0000

    test   // dynamic store

--- lib/fetchData/getCategory.tsx
+++ lib/fetchData/getCategory.tsx
@@ -4,6 +4,7 @@ import { userInfo } from '@/lib/userInfo'
 
 const getCategory = async (categoryId:string): Promise<Category> => {
   const {storeId} = await userInfo()
+  // dynamic store
   const URL = `${process.env.NEXT_PUBLIC_STORE_URL}${storeId}/categories`
   const response = await fetch(`${URL}/${categoryId}`)
   return response.json()
